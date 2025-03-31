0480b570534a2cf053d4a61a52b3a689d1a95c3f
unknown <nickoluoch1130@gmail.com>
2025-03-31 17:41:41 +0300

    adding the routing to pages

---
+++ src/pages/RootLayout.jsx
@@ -0,0 +1,5 @@
+export default function RootLayout(){
+    return (
+        <p>This is root layout</p>
+    )
+}
--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -1,13 +1,18 @@
+import RootLayout from "../pages/RootLayout";
 import Home from "../pages/Home";
 import Shop from "../pages/Shop";
 import CardPage from "../pages/CardPage";
 
  const routes = [
     {
-        path: "/",
-        element: <Home />,
-        
-      }  , { path: "/shop", element: <Shop /> },
-      { path: "/shop/:title", element: <CardPage /> },]
+      path: "/Shopping-Cart",
+      element: <RootLayout />,
+      children: [
+        { path: "", element: <Home /> },
+        { path: "shop", element: <Shop /> },
+        { path: "shop/:title", element: <CardPage /> },
+      ],
+    },
+  ]
 
 export default routes;
