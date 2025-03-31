886fd25a11a714e66a46e807a4006cface50c870
unknown <nickoluoch1130@gmail.com>
2025-03-31 16:32:29 +0300

    project initialization

---
+++ src/data/sneakers.js
@@ -0,0 +1,74 @@
+import sneak1 from '../assets/sneakers/sneak_1.png'
+import sneak2 from '../assets/sneakers/sneak_2.png'
+import sneak3 from '../assets/sneakers/sneak_3.png'
+import sneak4 from '../assets/sneakers/sneak_4.png'
+import sneak5 from '../assets/sneakers/sneak_5.png'
+import sneak6 from '../assets/sneakers/sneak_6.png'
+import sneak7 from '../assets/sneakers/sneak_7.png'
+import sneak8 from '../assets/sneakers/sneak_8.png'
+import sneak9 from '../assets/sneakers/sneak_9.png'
+import sneak10 from '../assets/sneakers/sneak_10.png'
+import sneak11 from '../assets/sneakers/sneak_11.png'
+
+const mainSneakers = [
+    {
+      title: 'Nike GT Cut 3',
+      price: 300,
+      image: sneak1,
+    },
+    {
+      title: 'Adidas AE 1',
+      price: 180,
+      image: sneak2,
+    },
+    {
+      title: 'Adidas Dame 9',
+      price: 140,
+      image: sneak3,
+    },
+    {
+      title: 'Anta KT 10',
+      price: 235,
+      image: sneak4,
+    },
+    {
+      title: 'Nike Sabrina 2',
+      price: 280,
+      image: sneak5,
+    },
+    {
+      title: 'Nike Book 1',
+      price: 180,
+      image: sneak6,
+    },
+  ]
+  
+  const otherSneakers = [
+    {
+      title: 'Anta Kai 1',
+      price: 165,
+      image: sneak7,
+    },
+    {
+      title: 'Way of Wade 11',
+      price: 405,
+      image: sneak8,
+    },
+    {
+      title: 'Curry 12',
+      price: 200,
+      image: sneak9,
+    },
+    {
+        title: 'Nike Ja 2',
+        price: 200,
+        image: sneak10,
+      },
+      {
+        title: 'Jordan Tatum 2',
+        price: 200,
+        image: sneak11,
+      },
+  ]
+  
+  export { mainSneakers, otherSneakers }
