8c1253251e2048422e2d1775a3617f479e4001c5
unknown <nickoluoch1130@gmail.com>
2025-03-31 16:52:26 +0300

    project initialization

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,5 @@
+export default function Shop(){
+    return (
+        <p>This is the shop page!</p>
+    )
+}
---
+++ src/route/Routes.jsx
@@ -0,0 +1,14 @@
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+
+ const routes = [{
+    path:'/',
+    element:<Home />
+},
+{
+    path:'/shop',
+    element:<Shop/>
+
+}]
+
+export default routes;
