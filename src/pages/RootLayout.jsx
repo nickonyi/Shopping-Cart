91fcc5f604b42e4f597ea527d4149791ee76d4b6
phatommobb <nickoluoch1130@gmail.com>
2025-04-01 12:07:24 +0300

    working on header

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,16 +1,26 @@
-import { Outlet } from "react-router-dom"
-import Header from "../components/Header"
-import Cart from "../components/Cart"
-import Footer from "../components/Footer"
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+import Cart from '../components/Cart';
+import Footer from '../components/Footer';
+import { useState } from 'react';
 
+export default function RootLayout() {
+  const [cartStatus, setCartStatus] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
 
-export default function RootLayout(){
-    return (
-       <>
-       <Header/>
-       <Outlet />
-       <Cart />
-       <Footer />
-       </>
-    )
+  const toggleCart = () => {
+    if (!cartStatus) {
+      setCartStatus(true);
+    } else {
+      setCartStatus(false);
+    }
+  };
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Cart />
+      <Footer />
+    </>
+  );
 }
