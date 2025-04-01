5b7b15fe34db89561fca7b6563d8276b26f7defa
phatommobb <nickoluoch1130@gmail.com>
2025-04-01 12:47:06 +0300

    working on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,15 @@
+import { ReactComponent as Logo } from '../assets/icons/logo.svg';
+import { ReactComponent as Cart } from '../assets/icons/cart.svg';
+import { ReactComponent as Magnify } from '../assets/icons/magnify.svg';
+import Nav from './Nav';
+
 export default function Header({ toggleCart, cartItems }) {
-  return <div>This is the header!!!</div>;
+  const computeAmount = () => {
+    return 'hello';
+  };
+  return (
+    <header>
+      <div className="container"></div>
+    </header>
+  );
 }
