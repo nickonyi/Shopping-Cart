10d2cc39c83b1e0f73cc8329582bae59a9bfd1d7
phatommobb <nickoluoch1130@gmail.com>
2025-04-04 17:49:52 +0300

    creating the card grid

--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -1,18 +1,18 @@
-import RootLayout from "../pages/RootLayout";
-import Home from "../pages/Home";
-import Shop from "../pages/Shop";
-import CardPage from "../pages/CardPage";
+import RootLayout from '../pages/RootLayout';
+import Home from '../pages/Home';
+import Shop from '../pages/Shop';
+import CardPage from '../pages/CardPage';
 
- const routes = [
-    {
-      path: "/Shopping-Cart",
-      element: <RootLayout />,
-      children: [
-        { path: "", element: <Home /> },
-        { path: "shop", element: <Shop /> },
-        { path: "shop/:title", element: <CardPage /> },
-      ],
-    },
-  ]
+const routes = [
+  {
+    path: '/Shopping-Cart',
+    element: <RootLayout />,
+    children: [
+      { path: '', element: <Home /> },
+      { path: 'shop', element: <Shop /> },
+      { path: 'shop/:title', element: <CardPage /> },
+    ],
+  },
+];
 
 export default routes;
