cb0588198478d4867ac36d8372236a275bef73a7
phatommobb <nickoluoch1130@gmail.com>
2025-04-11 16:32:30 +0300

    responsive view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import Logo from '../assets/icons/logo.svg?react';
 import Cart from '../assets/icons/cart.svg?react';
+import Hamburger from '../assets/icons/bars-solid.svg?react';
 import Magnify from '../assets/icons/magnify.svg?react';
 import Nav from './Nav';
 import { NavLink } from 'react-router-dom';
@@ -28,7 +29,7 @@ export default function Header({ toggleCart, cartItems }) {
       <div className={`nav-wrapper ${menuOpen ? 'open' : ''}`}>
         <Nav />
       </div>
-      <div className="container gap">
+      <div className={`container gap ${menuOpen ? 'open' : ''}`}>
         <div className="cart-icon" onClick={toggleCart}>
           <Cart className="icon" />
           <div className="cart-amount">{computeAmount()}</div>
@@ -38,7 +39,7 @@ export default function Header({ toggleCart, cartItems }) {
       </div>
 
       <button className="hamburger" onClick={handleToggle}>
-        &#9776;
+        <Hamburger className="burger" />
       </button>
     </header>
   );
